a8736d3aae40853f561e475d999508a3e4fbd39d
brenu <bvsousa64@gmail.com>
2020-07-09 02:10:53 -0300

    :rocket: Implement tomato gamification and add assets

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { FaPlay, FaPause } from "react-icons/fa";
+
 import "./custom.css";
 
+import tomato from "../../assets/tomato.png";
+
 export default function Main() {
-  const [tempo, setTempo] = useState(1500030);
+  const [tempo, setTempo] = useState(1500020);
   const [minutos, setMinutos] = useState(0);
   const [segundos, setSegundos] = useState(0);
   const [fase, setFase] = useState("trabalho");
   const [isPaused, setIsPaused] = useState(true);
+  const [sessions, setSessions] = useState(0);
+  const [tomatoes, setTomatoes] = useState([]);
 
   useEffect(() => {
     let tempoPivot = tempo;
@@ -40,6 +45,8 @@ export default function Main() {
     }
   }, [tempo, isPaused]);
 
+  useEffect(() => console.log(tomatoes), [tomatoes]);
+
   function handlePause() {
     if (isPaused === true) {
       setIsPaused(false);
@@ -60,6 +67,8 @@ export default function Main() {
 
   function handlePhaseChange() {
     if (fase === "trabalho") {
+      setSessions(sessions + 1);
+      handleTomatoPush();
       setFase("descanso");
       setTempo(300000);
     } else {
@@ -68,20 +77,34 @@ export default function Main() {
     }
   }
 
+  function handleTomatoPush() {
+    setTomatoes([
+      ...tomatoes,
+      <img key={sessions} src={tomato} className="tomato" />,
+    ]);
+  }
+
   return (
     <div className="container">
-      <div className="timer-container">
-        <h1 className="title">Hora do {fase}</h1>
-        <p>
-          {minutos}:{segundos > 9 ? segundos : "0" + segundos}
-        </p>
-        <div className="buttons-container">
-          <button className="button" onClick={handlePause}>
-            {isPaused === false ? <FaPause /> : <FaPlay />}
-          </button>
-          <button className="button" onClick={handleReset}>
-            Reset
-          </button>
+      <div className="center-container">
+        <div className="tomatoes-container">
+          {tomatoes.map((tomato) => {
+            return tomato;
+          })}
+        </div>
+        <div className="timer-container">
+          <h1 className="title">Hora do {fase}</h1>
+          <p>
+            {minutos}:{segundos > 9 ? segundos : "0" + segundos}
+          </p>
+          <div className="buttons-container">
+            <button className="button" onClick={handlePause}>
+              {isPaused === false ? <FaPause /> : <FaPlay />}
+            </button>
+            <button className="button" onClick={handleReset}>
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     </div>
